d810dc1dade2c23bf14e3403b6e2ff9143be7986
TharinduShantha98 <tharindushantha4@gmail.com>
2022-04-27 16:11:58 -0700

    create singlton class for db connection

--- 01-JSONProcessingHomeWork/src/CustomerServlet.java
+++ 01-JSONProcessingHomeWork/src/CustomerServlet.java
@@ -1,3 +1,5 @@
+import db.DbConnection;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -5,6 +7,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 @WebServlet(urlPatterns = "/customer")
 public class CustomerServlet extends HttpServlet {
@@ -14,6 +20,30 @@ public class CustomerServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        try {
+            Connection connection = DbConnection.getInstance().getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM customer");
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            if(resultSet.next()){
+                String id  = resultSet.getString(1);
+                String name  =  resultSet.getString(2);
+                String address =  resultSet.getString(3);
+                String salary  =  resultSet.getString(4);
+
+
+
+            }
+
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
 
     }
 }
